7a198a5e6655140517913f4c5e2fa4febd19522a
Васильева Мария Александровна <ip713s05@mwp02.csc.local>
2018-03-01 12:31:30 +0700

    create file chessboard, create programm

---
+++ chessboard.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+printf("8 r n b q k b n r \n");
+printf("7 p p p p p p p p \n");
+printf("6 \n");
+printf("5 \n");
+printf("4          \n");
+printf("3 \n");
+printf("2 P P P P P P P P \n");
+printf("1 R N B Q K B N R\n");
+printf("  a b c d e f g h \n");
+return 0;
+}
